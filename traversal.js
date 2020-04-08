9bf85360965ed2f1dafb08c4d8ceda4231b1f8c9
irfan-fauzi <irfanwintech@yahoo.com>
2020-04-08 19:55:45 +0700

    traversal2

--- traversal.js
+++ traversal.js
@@ -1,7 +1,7 @@
 // let card = document.querySelector('.card');
 let close = document.querySelectorAll('.close');
-close.forEach(function (e) {
-    e.addEventListener('click', function (event) {
+close.forEach(function (el) {
+    el.addEventListener('click', function (event) {
         event.target.parentElement.style.display = 'none';
     })
 
